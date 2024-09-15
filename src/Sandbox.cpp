f6c72959ccf608caf1ed262ebcc90960e20bfb78
David Cottrell <david.cottrell@ukaea.uk>
2024-09-15 21:40:57 +0100

    Add log messages

--- src/Sandbox.cpp
+++ src/Sandbox.cpp
@@ -4,10 +4,13 @@ class Sandbox : public AE::Application
 {
 public:
     Sandbox() {}
-    ~Sandbox() {}
+    ~Sandbox() {
+        AE_INFO("Application shutdown");
+    }
 };
 
 AE::Application *AE::CreateApplication()
 {
+    AE_INFO("Starting application...");
     return new Sandbox();
 }
