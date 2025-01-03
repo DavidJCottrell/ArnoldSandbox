ddc1a4ac5c7e1db8053fbeeab1711815c3422e47
David Cottrell <davidjcottrell@icloud.com>
2025-01-03 16:53:12 +0000

    Use AE Keycode

--- src/ExampleLayer.cpp
+++ src/ExampleLayer.cpp
@@ -4,6 +4,10 @@ ExampleLayer::ExampleLayer() = default;
 
 void ExampleLayer::OnUpdate()
 {
+    if (AE::Core::Input::IskeyPressed(AE_KEY_TAB))
+    {
+        AE_INFO("TAB KEY PRESSED");
+    }
 }
 
 void ExampleLayer::OnEvent(AE::Events::Event& event)
